b8f91eb75653649fb05e0c692d30a28b60e02b75
RedSkulHYDRA <ishitgupta11196@gmail.com>
2023-04-06 14:27:54 +0530

    Removed device specific trees since they are now in manifest

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -6,33 +6,9 @@ end="\033[0m"
 echo -e "${color}Applying patches${end}"
 sleep 1
 
-# Clone Common Tree
-git clone https://github.com/RedSkulHYDRA/device_oneplus_sm8150-common -b yaap-custom device/oneplus/sm8150-common
-
 # Kernel Requirements (Inlining Neptune)
 git clone https://github.com/yaap/prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9.git prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9
-git clone https://gitlab.com/Flamingo-OS/dora_clang.git -b A13 /home/redskul/yaap/prebuilts/clang/host/linux-x86/clang-dora
-
-# Clone vendor
-git clone https://github.com/yaap/vendor_oneplus_hotdogb -b thirteen vendor/oneplus/hotdogb
-git clone https://github.com/yaap/vendor_oneplus_sm8150-common.git -b thirteen vendor/oneplus/sm8150-common
-
-# Hardware
-git clone https://github.com/yaap/hardware_oneplus -b thirteen hardware/oneplus
-
-# Qcomm
-git clone https://github.com/yaap/device_qcom_common -b thirteen device/qcom/common
-git clone https://gitlab.com/yaosp/vendor_qcom_common -b thirteen vendor/qcom/common
-
-# Clone other Devices trees
-git clone https://github.com/RedSkulHYDRA/device_oneplus_hotdog.git -b yaap-custom device/oneplus/hotdog
-git clone https://github.com/RedSkulHYDRA/device_oneplus_guacamole.git -b yaap-custom device/oneplus/guacamole
-git clone https://github.com/RedSkulHYDRA/device_oneplus_guacamoleb.git -b yaap-custom device/oneplus/guacamoleb
-
-# Clone other vendor trees
-git clone https://github.com/yaap/vendor_oneplus_hotdog.git -b thirteen vendor/oneplus/hotdog
-git clone https://github.com/yaap/vendor_oneplus_guacamole.git -b thirteen vendor/oneplus/guacamole
-git clone https://github.com/yaap/vendor_oneplus_guacamoleb.git -b thirteen vendor/oneplus/guacamoleb
+git clone https://gitlab.com/Flamingo-OS/dora_clang.git -b A13 prebuilts/clang/host/linux-x86/clang-dora
 
 # DOLBY
 git clone https://github.com/RedSkulHYDRA/hardware_dolby.git hardware/dolby
@@ -42,4 +18,4 @@ git clone https://github.com/RedSkulHYDRA/vendor_icons.git vendor/icons
 
 # Config.xml 
 rm -rf vendor_yaap/overlay/frameworks/base/core/res/res/values/config.xml
-git clone https://github.com/RedSkulHYDRA/vendor_yaap.git -b yaap-custom vendor_yaap/overlay/frameworks/base/core/res/res/values/
+git clone https://github.com/RedSkulHYDRA/vendor_yaap.git -b yaap-custom vendor_yaap/overlay/frameworks/base/core/res/res/values/config.xml
