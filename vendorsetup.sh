bab4f7356ac4159c996addd555797dd7386ab7ee
RedSkulHYDRA <ishitgupta11196@gmail.com>
2023-04-06 23:30:40 +0530

    Removed cloning from vendorsetup.sh and moveded to clone.sh for manual initiation

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,25 +1,3 @@
-# ROM source patches
 
-color="\033[0;32m"
-end="\033[0m"
-
-echo -e "${color}Applying patches${end}"
-sleep 1
-
-# Kernel Requirements (Inlining Neptune)
-git clone https://github.com/yaap/prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9.git prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9
-git clone https://gitlab.com/Flamingo-OS/dora_clang.git -b A13 prebuilts/clang/host/linux-x86/clang-dora
-
-# DOLBY
-git clone https://github.com/RedSkulHYDRA/hardware_dolby.git hardware/dolby
-
-# Themed Icons
-git clone https://github.com/RedSkulHYDRA/vendor_icons.git vendor/icons
-
-# Config.xml 
-rm -rf vendor/yaap/overlay/frameworks/base/core/res/res/values/config.xml
-wget https://raw.githubusercontent.com/RedSkulHYDRA/vendor_yaap/yaap-custom/config.xml -P /home/redskul/yaap/vendor/yaap/overlay/frameworks/base/core/res/res/values
-
-# Qcomm
-git clone https://github.com/yaap/device_qcom_common -b thirteen device/qcom/common
-git clone https://gitlab.com/yaosp/vendor_qcom_common -b thirteen vendor/qcom/common
+cd device/oneplus/hotdogb
+chmod +x clone.sh
