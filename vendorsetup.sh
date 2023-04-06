5e532973d78934ffcbc1d7495d9015cc53a8f89b
RedSkulHYDRA <ishitgupta11196@gmail.com>
2023-04-06 10:34:14 +0530

    Cleaned up vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -6,33 +6,21 @@ end="\033[0m"
 echo -e "${color}Applying patches${end}"
 sleep 1
 
-# Remove unwanted apps 
-rm -rf packages/apps/Aperture
-rm -rf packages/apps/Camera2
-rm -rf packages/apps/Matlog
-
-
 # Clone Common Tree
-
 git clone https://github.com/RedSkulHYDRA/device_oneplus_sm8150-common -b yaap-custom device/oneplus/sm8150-common
 
-# Clone Kernel (Inlining Neptune)
-rm -rf packages/apps/KProfiles
+# Kernel Requirements (Inlining Neptune)
 git clone https://github.com/yaap/prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9.git prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9
 git clone https://gitlab.com/Flamingo-OS/dora_clang.git -b A13 /home/redskul/yaap/prebuilts/clang/host/linux-x86/clang-dora
-git clone https://github.com/Codecity001/Neptune_kernel_sm8150_oneplus.git -b inline-erofs kernel/oneplus/sm8150
 
 # Clone vendor
-
 git clone https://github.com/yaap/vendor_oneplus_hotdogb -b thirteen vendor/oneplus/hotdogb
 git clone https://github.com/yaap/vendor_oneplus_sm8150-common.git -b thirteen vendor/oneplus/sm8150-common
 
 # Hardware
-
 git clone https://github.com/yaap/hardware_oneplus -b thirteen hardware/oneplus
 
 # Qcomm
-
 git clone https://github.com/yaap/device_qcom_common -b thirteen device/qcom/common
 git clone https://gitlab.com/yaosp/vendor_qcom_common -b thirteen vendor/qcom/common
 
@@ -45,7 +33,6 @@ git clone https://github.com/RedSkulHYDRA/device_oneplus_guacamoleb.git -b yaap-
 git clone https://github.com/yaap/vendor_oneplus_hotdog.git -b thirteen vendor/oneplus/hotdog
 git clone https://github.com/yaap/vendor_oneplus_guacamole.git -b thirteen vendor/oneplus/guacamole
 git clone https://github.com/yaap/vendor_oneplus_guacamoleb.git -b thirteen vendor/oneplus/guacamoleb
-
 
 # DOLBY
 git clone https://github.com/RedSkulHYDRA/hardware_dolby.git hardware/dolby
